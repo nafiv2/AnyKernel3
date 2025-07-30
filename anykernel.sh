a2232fc39b2eaddabf9335d2ddd4895301fdd0ed
nafiv2 <miftahunfatah94@gmail.com>
2025-07-30 11:19:04 +0000

    anykernel : adapt to samsung a22 5g
    Signed-off-by: nafiv2 <miftahunfatah94@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -5,17 +5,17 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=
-do.devicecheck=0
+kernel.string=A226B-AZURE
+do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
-device.name3=
-device.name4=
-device.name5=
+device.name1=a22x
+device.name2=a226b
+device.name3=A226B
+device.name4=A226
+device.name5=a226
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -30,7 +30,7 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=
+BLOCK=/dev/block/platform/bootdevice/by-name/boot;
 IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
